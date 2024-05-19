04cc60dc34bdf19986f7b295801f819dea130dfa
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-05-19 17:10:19 +0300

    cf task

---
+++ prj.codeforces/1969a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int a[50];
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    int m = 3;
+    for (int i = 0; i < n; ++i) {
+      if (i + 1 == a[a[i] - 1]) {
+        m = 2;
+        break;
+      }
+    }
+    cout << m << "\n";
+  }
+}
