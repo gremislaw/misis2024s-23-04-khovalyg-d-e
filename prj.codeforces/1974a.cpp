af8636a5013df0285f67ddb93e56a14c48ad1b95
Dazhy Khovalyg <hovalyg_d@mail.ru>
2024-05-21 17:36:05 +0300

    phone task cf

---
+++ prj.codeforces/1974a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+        
+        cout << max((y+1)/2 , (4*y+x+14)/15) << endl;
+        
+    }
+}
